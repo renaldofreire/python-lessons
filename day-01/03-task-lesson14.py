a3f53fa90ce056b1afc7f20dae935afd396712f0
Renaldo Freire <eurenaldo@gmail.com>
2023-09-28 10:23:04 -0300

    Terminar day-01

--- day-01/03-task-lesson14.py
+++ day-01/03-task-lesson14.py
@@ -6,6 +6,18 @@ b = input("b:")
 ###################
 # escrever aqui ##
 
+# convert inteiro
+# a_int = int(a)
+# b_int = int(b)
+
+# # converte string novamente, agora mudando valores
+# a = str(b_int)
+# b = str(a_int)
+
+# Simple way:
+c = a
+a = b
+b = c
 
 ###################
 # Não mudar
---
+++ day-01/04-brand-name-generator.py
@@ -0,0 +1,11 @@
+# Band Name Generator
+
+print("Bem vindo ao Gerador de nomes de bandas.")
+
+city_name = input("Qual cidade você nasceu? ")
+print(city_name)
+
+pets_name = input("Me diga um nome de um animal: ")
+print(pets_name)
+
+print("O nome da sua banda é " + city_name + "'s " + pets_name + ".")
